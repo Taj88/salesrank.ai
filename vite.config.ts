51734e8f08e6ab4a61527f9c516075a126166abc
Taj88 <taj16@cse.pstu.ac.bd>
2025-05-02 19:00:48 +0600

    feat: configure build settings for output directory and cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,8 @@ export default defineConfig({
     server: {
         port: 3000,
     },
+    build: {
+        outDir: 'build', // Set the output directory to 'build'
+        emptyOutDir: true, // Empty the output directory before building
+    },
 });
